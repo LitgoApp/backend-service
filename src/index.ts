61146768fe0b3a085cd791d3d5094a19e194e44a
Blair McAlpine <blair.mcalpine77@gmail.com>
2023-06-01 18:35:03 -0400

    added prisma

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
 
@@ -5,9 +6,15 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
+const prisma = new PrismaClient();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.get('/users', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
 });
 
 app.listen(port, () => {
