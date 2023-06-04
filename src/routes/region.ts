95be3ea827179cac07dbb141290582a3c4434593
erfan7emz <68251969+erfan7emz@users.noreply.github.com>
2023-06-04 12:16:40 -0400

    region api

--- src/routes/region.ts
+++ src/routes/region.ts
@@ -4,76 +4,76 @@ const prisma = require("@prisma/client").PrismaClient;
 
 router.get('/', async (req: Request, res: Response) => {
     try {
-        const rewards = await prisma.reward.findMany()
-        res.json(rewards)
+        const regions = await prisma.region.findMany()
+        res.json(regions)
     } catch (error) {
-        res.status(500).json({ message: 'An error occurred while getting all rewards'  })
+        res.status(500).json({ message: 'An error occurred while getting all regions' })
     }
 });
 
 router.get('/:id', async (req: Request, res: Response) => {
     try {
         const { id } = req.params;
-        const reward = await prisma.reward.findUnique({
+        const region = await prisma.region.findUnique({
             where: {
                 id: Number(id)
             }
         })
-        res.json(reward)
+        res.json(region)
     } catch (error) {
-        res.status(500).json({ message: 'An error occurred while getting a reward'  })
+        res.status(500).json({ message: 'An error occurred while getting a region' })
     }
 });
 
 router.post('/', async (req: Request, res: Response) => {
     try {
-        const result = await prisma.reward.create({
+        const result = await prisma.region.create({
             data: {
-                name: req.body.name,
-                cost: req.body.cost,
-                description: req.body.description,
+                regionId: req.body.regionId,
+                municipalityId: req.body.municipalityId,
+                points: req.body.points,
                 createdAt: req.body.createdAt,
                 updatedAt: req.body.updatedAt
             },
         })
         res.json(result)
     } catch (error) {
-        res.status(500).json({ message: 'An error occurred while creating a reward'  })
+        res.status(500).json({ message: 'An error occurred while creating a region' })
     }
 });
 
 router.put('/:id', async (req: Request, res: Response) => {
     try {
         const { id } = req.params;
-        const result = await prisma.reward.update({
+        const result = await prisma.region.update({
             where: {
                 id: Number(id)
             },
             data: {
-                name: req.body.name,
-                cost: req.body.cost,
-                description: req.body.description,
+                regionId: req.body.regionId,
+                municipalityId: req.body.municipalityId,
+                points: req.body.points,
                 createdAt: req.body.createdAt,
                 updatedAt: req.body.updatedAt
             },
         })
         res.json(result)
     } catch (error) {
-        res.status(500).json({ message: 'An error occurred while updating a reward'  })
+        res.status(500).json({ message: 'An error occurred while updating a region' })
     }
 });
 
 router.delete('/:id', async (req: Request, res: Response) => {
     try {
         const { id } = req.params;
-        const result = await prisma.reward.delete({
+        const result = await prisma.region.delete({
             where: {
                 id: Number(id)
             }
         })
         res.json(result)
     } catch (error) {
-        res.status(500).json({ message: 'An error occurred while deleting a reward'  })
+        res.status(500).json({ message: 'An error occurred while deleting a region' })
     }
 });
 
